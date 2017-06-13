cf54bbe3c169e4dae9a4ac680fbf662ef94fe878
Your Name <you@example.com>
2017-06-13 19:55:43 +0100

    1st commit

---
+++ lib/shutter3.rb
@@ -0,0 +1,95 @@
+#!/usr/bin/env ruby
+
+# file: shutter3.rb
+
+# description: Uses the command `hcidump --raw` to detect the bluetooth 
+#              button events for a wireless bluetooth remote control
+#              shutter button.
+
+# see also: https://github.com/kinnalru/bluebutton
+
+note: To use hcidump, `apt-get install bluez-hcidump`. 
+
+
+class Shutter3
+
+  def initialize(bdid='', debug: false)
+    @bdid = bdid.split(':').reverse.join(' ')
+    @debug = debug
+  end
+
+  def start()
+
+    button = nil
+    found = 0
+
+    IO.popen('sudo hcidump --raw').each_line do |x| 
+
+      case x
+      when /#{@bdid}/
+
+        found += 1
+
+        if found > 2 then
+          on_connect()
+          next
+        end
+      when /> 04 05 04 00 40 00 08 \n/
+        on_disconnect()
+        next
+      end
+
+      case x[/> 02 40 20 09 00 05 00 04 00 1B (\w+ \w+ \w+ \w+)(?= \n$)/,1]
+      when "17 00 00 28"
+        on_android_keydown()
+        on_android_keypress()
+        button = :android
+      when "17 00 00 00"
+        on_android_keyup()
+        button = nil
+      when "13 00 01 00"
+        next if button == :android
+        on_ios_keydown()
+        on_ios_keypress()
+      when "13 00 00 00"
+        next if button == :android
+        on_ios_keyup()
+      when ""
+      end
+      puts x.inspect if @debug
+    end
+
+  end
+
+  protected
+
+  def on_android_keypress()
+    puts 'android button pressed'
+  end
+
+  def on_android_keydown()
+  end
+
+  def on_android_keyup()
+  end
+
+  def on_connect()
+    puts 'connected to ' + @bdid
+  end
+
+  def on_disconnect()
+    puts @bdid + ' disconnected'
+  end
+
+  def on_ios_keypress()
+    puts 'iOS button pressed'
+  end
+
+  def on_ios_keydown()
+  end
+
+  def on_ios_keyup()
+  end
+
+end
+
---
+++ shutter3.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+  s.name = 'shutter3'
+  s.version = '0.1.0'
+  s.summary = 'Uses the command `hcidump --raw` (see bluez-hcidump) to ' + 
+      'detect the bluetooth button events for a wireless bluetooth remote ' + 
+      'control shutter button.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/shutter3.rb']
+  s.signing_key = '../privatekeys/shutter3.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/shutter3'
+end
